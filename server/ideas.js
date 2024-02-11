0c7ea73249618d1824f279215b64ef0f31f5f84a
Taylor Ericson <116030067+tkericson@users.noreply.github.com>
2024-02-10 22:26:31 -0600

    complete get by id route for ideas path

--- server/ideas.js
+++ server/ideas.js
@@ -12,6 +12,16 @@ const {
 
 const checkMillionDollarIdea = require("./checkMillionDollarIdea");
 
+ideasRouter.param("id", (req, res, next, id) => {
+  const idea = getFromDatabaseById("ideas", id);
+  if (idea) {
+    req.idea = idea;
+    next();
+  } else {
+    res.status(404).send();
+  }
+});
+
 //GET array of all ideas
 ideasRouter.get("/", (req, res, next) => {
   res.send(getAllFromDatabase("ideas"));
@@ -22,3 +32,12 @@ ideasRouter.post("/", (req, res, next) => {
   const newIdea = addToDatabase("ideas", req.body);
   res.status(201).send(newIdea);
 });
+
+//GET a single idea by id
+ideasRouter.get("/:id", (req, res, next) => {
+  res.send(req.idea);
+});
+
+//PUT update a single id by id
+
+//DELETE a signle id by id
